19e2e1bf53a8177aa7f5a7ae6c541cbffede1a43
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2023-11-26 13:54:52 -0500

    should revisit traffic.cpp

--- USACO/Guide/Simulation/cowsignal.py
+++
@@ -1,8 +0,0 @@
-with open('cowsignal.in', 'r') as fin:
-    M, N, K = map(int, fin.readline().split())
-    signal = [fin.readline().strip() for _ in range(M)]
-
-with open('cowsignal.out', 'w') as fout:
-    for row in signal:
-        for _ in range(K):
-            fout.write(''.join([char * K for char in row]) + '\n')
--- USACO/Guide/Simulation/traffic.cpp
+++ USACO/Guide/Simulation/traffic.cpp
@@ -12,5 +12,47 @@ int main() {
     int N;
     cin >> N;
 
-    
+    vector<string> actions(N);
+    vector<vector<int>> ranges(N);
+    for (int i=0; i<N; i++) {
+        vector<int> range(2);
+        cin >> actions[i] >> range[0] >> range[1];
+
+        ranges[i] = range;
+    }
+
+    int lower_range_start = 0;
+    int upper_range_start = 1e9;
+
+    for (int i=N-1; i>=0; i--) {
+        if (actions[i] == "none") {
+            lower_range_start = max(lower_range_start, ranges[i][0]);
+            upper_range_start = min(upper_range_start, ranges[i][1]);
+        } else if (actions[i] == "off") {
+            lower_range_start += ranges[i][0];
+            upper_range_start += ranges[i][1];
+        } else {
+            lower_range_start -= min(ranges[i][1], lower_range_start);
+            upper_range_start -= ranges[i][0], lower_range_start;
+        }
+    }
+
+    int lower_range_end = 0;
+    int upper_range_end = 1e9;
+
+    for (int i=0; i<N; i++) {
+        if (actions[i] == "none") {
+            lower_range_end = max(lower_range_end, ranges[i][0]);
+            upper_range_end = min(upper_range_end, ranges[i][1]);
+        } else if (actions[i] == "on") {
+            lower_range_end += ranges[i][0];
+            upper_range_end += ranges[i][1];
+        } else {
+            lower_range_end -= min(ranges[i][1], lower_range_end);
+            upper_range_end -= ranges[i][0];
+        }
+    }
+
+    cout << lower_range_start << ' ' << upper_range_start << '\n';
+    cout << lower_range_end << ' ' << upper_range_end << '\n';
 }
