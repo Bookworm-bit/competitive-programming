a868ab8cb3e3e224e79608f379d79c55e2d1eed3
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2023-12-17 18:41:21 -0500

    eeee, some empty files committed too

--- USACO/Guide/Basic Complete Search/liars.cpp
+++ USACO/Guide/Basic Complete Search/liars.cpp
@@ -1,11 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void setIO(string s) {
-    freopen((s + ".in").c_str(), "r", stdin);
-    freopen((s + ".out").c_str(), "w", stdout);
-}
-
 int main() {
     int N;
     cin >> N;
