cd1fb74c8bd1e60a80aa0a8acdc97dbcac96a931
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2024-01-29 09:32:20 -0500

    haelp

--- USACO/Guide/template.cpp
+++ USACO/Guide/template.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+typedef long long ll;
 
 void setIO(string s) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
     freopen((s + ".in").c_str(), "r", stdin);
     freopen((s + ".out").c_str(), "w", stdout);
 }
 
 int main() {
     setIO("");
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+   
 }
