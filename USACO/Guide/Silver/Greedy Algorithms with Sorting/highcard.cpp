c914916538282025e67086e6e5892651db89c425
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2024-02-28 15:19:31 -0500

    did some cp today :D

---
+++ USACO/Guide/Silver/Greedy Algorithms with Sorting/highcard.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void setIO(string s) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+int main() {
+    setIO("highcard");
+
+	int N;
+	cin >> N;
+
+	map<int, int> elsie;
+	for (int i=1; i<2*N+1; i++) {
+		elsie[i] = (int) 1e9 + 1;
+	}
+
+	for (int i=0; i<N; i++) {
+		int temp;
+		cin >> temp;
+
+		elsie[temp] = 2*N - temp;
+	}
+
+	int ret = 0;
+	int sub = 0;
+	int bessie = 0;
+	for (int i=2*N; i>0; i--) {
+		if (elsie[i] == (int) 1e9 + 1) {
+			bessie++;
+		} else {
+			if (bessie - sub > 0) {
+				ret++;
+				sub++;
+			}
+		}
+	}
+
+	cout << ret << '\n';
+}
