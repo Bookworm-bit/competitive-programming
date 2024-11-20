48a10a510c71c616ae53f631e26223fba51467f7
Eric Zhang <81590955+Bookworm-bit@users.noreply.github.com>
2024-11-20 10:30:20 -0500

    Update buildingteams.cpp
    woah actual solve??? i hate impl

--- USACO/Guide/Silver/Graph Traversal/buildingteams.cpp
+++ USACO/Guide/Silver/Graph Traversal/buildingteams.cpp
@@ -11,6 +11,57 @@ void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited, vector<int>&
 		}
 	}
 }
+#include <bits/stdc++.h>
+using namespace std;
+
+int MAX_N = 100000;
+vector<vector<int>> adj(MAX_N);
+vector<bool> visited(MAX_N);
+vector<int> colors(MAX_N);
+bool bipartite = true;
+
+void dfs(int node, int color) {
+	colors[node] = color;
+	visited[node] = true;
+
+	for (auto c : adj[node]) {
+		if (!visited[c]) {
+			dfs(c, 1-color);
+		} else {
+			if (colors[c] == color) {
+				bipartite = false;
+			}
+		}
+	}
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	for (int i=0; i<m; i++) {
+		int a, b;
+		cin >> a >> b;
+		a--; b--;
+
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+
+	for (int i=0; i<n; i++) {
+		if (!visited[i]) {
+			dfs(i, 0);
+		}
+	}
+
+	if (bipartite) {
+		for (int i=0; i<n; i++) {
+			cout << colors[i] + 1 << ((i == n-1) ? '\n' : ' ');
+		}
+	} else {
+		cout << "IMPOSSIBLE" << '\n';
+	}
+}
 
 // check if bipartite is sol
 
